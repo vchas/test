34db1bf491fdc95a1bae019d386e637979c19707
vchas <vchas@users.noreply.github.com>
2015-10-11 21:32:15 +0300

    Create Function_Pitch&Roll_Acceleration_xyz_.ino

---
+++ Function_Pitch&Roll_Acceleration_xyz_.ino
@@ -0,0 +1,18 @@
+// x --> x Acceleration (g)
+// y --> y Acceleration (g)
+// z --> z Acceleration (g)
+
+void printOrientation(float x, float y, float z)
+{
+  float pitch, roll;
+  
+  pitch = atan2(x, sqrt(y * y) + (z * z));
+  roll = atan2(y, sqrt(x * x) + (z * z));
+  pitch *= 180.0 / PI;
+  roll *= 180.0 / PI;
+  
+  Serial.print("Pitch, Roll: ");
+  Serial.print(pitch, 2);
+  Serial.print(", ");
+  Serial.println(roll, 2);
+}
